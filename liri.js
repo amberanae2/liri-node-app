0c9e0dcbdfed0e47df160ad1b993860ad59d7cf9
Amber Phillips <amber.ranae2@gmail.com>
2018-04-10 19:32:09 -0500

    revised

--- liri.js
+++ liri.js
@@ -1,1 +1,21 @@
 require("dotenv").config();
+
+var request = require("request");
+
+var Twitter = require('twitter');
+var client = new Twitter({
+  consumer_key: 'z8jDcGJMAWXQf14kk2NWsgXai',
+  consumer_secret: 'gNbcimUcgUcmCLYzMYdWdsTSvGxgIixpBaCcuRQaavqHBHziQ5',
+  access_token_key: '981230077089284096-CSFCZ0WOguVcSZ5TxReuNIA9d2CObig',
+  access_token_secret: '9i5mxfGdK68xXQrM5z1zz0c9AsTNkimWFo2dTb5Tylnel'
+});
+
+var mytweets = process.argv[2]
+
+
+
+
+
+  
+
+  
